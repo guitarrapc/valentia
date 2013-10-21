22b0d2d362b77f2cea9be413e803eb145a41b2da
guitarrapc <cherishacquire@outlook.com>
2013-10-21 22:02:52 +0900

    added Chocolatey Installer sample by PowerShell

---
+++ example/Deployment/Application/Task-InstallChocolatey.ps1
@@ -0,0 +1,16 @@
+task Task-InstallChocolatey -action {
+
+    # installation
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+    
+    # Environment Setip
+    $envChocolatey = "%systemdrive%\chocolatey\bin;"
+    $envChocolateyRoot = Join-Path (Get-Item ([System.Environment]::SystemDirectory)).Root "chocolatey\bin"
+    if (-not($env:Path -match $envChocolatey) -or ($env:Path -match $envChocolateyRoot))
+    {
+        $currentEnvPath = $env:Path
+        $newEnvPath = $env:Path + "$envChocolatey"
+        [System.Environment]::SetEnvironmentVariable("Path",$newEnvPath,"user")
+    }
+
+}
