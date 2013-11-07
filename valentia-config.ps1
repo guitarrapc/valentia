37e0ce037c088f4a01e4ab356e98547418de5aa5
guitarrrapc <i.yoshizaki@grani.jp>
2013-11-08 05:54:38 +0900

    remove context.push

--- valentia-config.ps1
+++ valentia-config.ps1
@@ -26,20 +26,17 @@ $valentia.PSDrive = "V:" # Set Valentia Mapping Drive with SMBMapping
 $valentia.deployextension = ".ps1" # contains default DeployGroup file extension
 $valentia.wsmanSessionlimit = 22 # Set PSRemoting WSman limit prvention threshold
 
-
 # Define Prefix for Deploy Client NetBIOS name
 $valentia.prefix = New-Object psobject -property @{
     hostName = "web"
     ipstring = "ip"
 }
 
-
 # Define External program path
 $valentia.fastcopy = New-Object psobject -property @{
     folder = "C:\Program Files\FastCopy"
     exe = "FastCopy.exe"
 }
-
 
 # contains default Path configuration, can be overriden in ($valentia.defaultconfigurationfile) in directory with valentia.psm1 or in directory with current task script
 $valentia.RootPath = "C:\Deployment"
@@ -53,27 +50,9 @@ $valentia.BranchFolder = New-Object psobject -property @{
     Utils = "Utils"
 }
 
-
 # Set Valentia Log
 $valentia.log = New-Object psobject -property @{
     path = "C:\Logs\Deployment"
     name = "deploy"
     extension = ".log"
 }
-
-
-# contains context for default.
-$valentia.context.push(
-    @{
-        executedTasks = New-Object System.Collections.Stack;
-        callStack = New-Object System.Collections.Stack;
-        originalEnvPath = $env:Path;
-        originalDirectory = Get-Location;
-        originalErrorActionPreference = $global:ErrorActionPreference;
-        name = $valentia.name
-        version = $valentia.version
-        supportWindows = $valentia.supportWindows
-        tasks = @{}
-        includes = New-Object System.Collections.Queue;
-    }
-)
