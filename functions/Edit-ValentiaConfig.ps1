ae13c47fd6f27cb3afaa2c5c25b5a93b613aa573
guitarrapc <cherishacquire@outlook.com>
2013-11-15 02:06:43 +0900

    Added Edit-ValentiaConfig, Show-ValentiaConfig Cmdlet.

---
+++ functions/Edit-ValentiaConfig.ps1
@@ -0,0 +1,43 @@
+﻿function Edit-ValentiaConfig
+{
+<#
+.Synopsis
+   Edit Valentia Config in Console
+.DESCRIPTION
+   Read config and edit in the console
+.EXAMPLE
+   Edit-ValentiaConfig
+#>
+
+    param
+    (
+        [parameter(
+            mandatory = 0,
+            position = 0)]
+        [string]
+        $configName = (Join-Path $valentia.modulePath $valentia.defaultconfigurationfile),
+
+        [parameter(
+            mandatory = 0,
+            position = 1)]
+        [switch]
+        $NoProfile
+    )
+
+    if (Test-Path $configName)
+    {
+        if ($NoProfile)
+        {
+            PowerShell_ise.exe -File $configName
+        }
+        else
+        {
+            PowerShell_ise.exe $configName
+        }
+    }
+    else
+    {
+        Write-Verbose ("Could not found configuration file '{0}'.")
+    }
+
+}
---
+++ functions/Show-ValentiaConfig.ps1
@@ -0,0 +1,37 @@
+﻿function Show-ValentiaConfig
+{
+<#
+.Synopsis
+   Show Valentia Config in Console
+.DESCRIPTION
+   Read config and show in the console
+.EXAMPLE
+   Show-ValentiaConfig
+#>
+
+    param
+    (
+        [parameter(
+            mandatory = 0,
+            position = 0)]
+        [string]
+        $configName = (Join-Path $valentia.modulePath $valentia.defaultconfigurationfile),
+
+        [parameter(
+            mandatory = 0,
+            position = 1)]
+        [ValidateSet("Ascii", "BigEndianUnicode", "Byte", "Default","Oem", "String", "Unicode", "Unknown", "UTF32", "UTF7", "UTF8")]
+        [string]
+        $encoding = "default"
+    )
+
+    if (Test-Path $configName)
+    {
+        Get-Content -Path $configName -Encoding $encoding
+    }
+    else
+    {
+        Write-Verbose ("Could not found configuration file '{0}'.")
+    }
+
+}
