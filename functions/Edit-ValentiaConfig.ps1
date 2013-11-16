a45a2bcbfa798e2f2b142f2e61330e208a699612
guitarrrapc <i.yoshizaki@grani.jp>
2013-11-17 01:00:10 +0900

    change parameter name. fix missing config path for string format when config path not exist.

--- functions/Edit-ValentiaConfig.ps1
+++ functions/Edit-ValentiaConfig.ps1
@@ -15,7 +15,7 @@
             mandatory = 0,
             position = 0)]
         [string]
-        $configName = (Join-Path $valentia.modulePath $valentia.defaultconfigurationfile),
+        $configPath = (Join-Path $valentia.modulePath $valentia.defaultconfigurationfile),
 
         [parameter(
             mandatory = 0,
@@ -24,20 +24,20 @@
         $NoProfile
     )
 
-    if (Test-Path $configName)
+    if (Test-Path $configPath)
     {
         if ($NoProfile)
         {
-            PowerShell_ise.exe -File $configName
+            PowerShell_ise.exe -File $configPath
         }
         else
         {
-            PowerShell_ise.exe $configName
+            PowerShell_ise.exe $configPath
         }
     }
     else
     {
-        Write-Verbose ("Could not found configuration file '{0}'.")
+        Write-Verbose ("Could not found configuration file '{0}'." -f $configPath)
     }
 
 }
