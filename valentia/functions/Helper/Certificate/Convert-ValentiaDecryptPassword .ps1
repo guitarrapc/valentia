6e70490cc3451b01099f9666fcf64e4e29e84c24
guitarrapc <cherishacquire@outlook.com>
2014-05-22 19:11:26 +0900

    Added Password Encrypt / Decrypt through Certificate pfx/cert

---
+++ valentia/functions/Helper/Certificate/Convert-ValentiaDecryptPassword .ps1
@@ -0,0 +1,66 @@
+﻿#Requires -Version 3.0
+
+#-- Helper for certificate --#
+
+function Convert-ValentiaDecryptPassword 
+{
+    param
+    (
+        [parameter(
+            mandatory = 1,
+            position  = 0,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $EncryptedKey, 
+
+        [parameter(
+            mandatory = 0,
+            position  = 1,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $thumprint = $valentia.certificate.Encrypt.ThumPrint,
+
+        [parameter(
+            mandatory = 0,
+            position  = 1,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $certPath = $valentia.certificate.Encrypt.CertPath
+    )
+
+    process
+    {
+        foreach ($cred in $Credential)
+        {
+            $EnvelopedCms = New-Object Security.Cryptography.Pkcs.EnvelopedCms
+            $EnvelopedCms.Decode([convert]::FromBase64String($EncryptedKey))
+            $EnvelopedCms.Decrypt($Cert)
+            [Text.Encoding]::UTF8.GetString($EnvelopedCms.ContentInfo.Content)
+        }
+    }
+
+    begin
+    {
+        try
+        {
+            Add-type –AssemblyName System.Security
+        }
+        catch
+        {
+        }
+
+        $Path = Join-Path $certPath $thumprint
+        if (Test-Path $Path)
+        {
+            $Cert = Get-Item $Path
+        }
+        else
+        {
+            Write-Warning ("Certification not found exception!! Cert: '{0}'" -f $Path)
+        }
+    }
+}
---
+++ valentia/functions/Helper/Certificate/Convert-ValentiaEncryptPassword .ps1
@@ -0,0 +1,67 @@
+﻿#Requires -Version 3.0
+
+#-- Helper for certificate --#
+
+function Convert-ValentiaEncryptPassword 
+{
+    param
+    (
+        [parameter(
+            mandatory = 1,
+            position  = 0,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [pscredential[]]
+        $Credential, 
+
+        [parameter(
+            mandatory = 0,
+            position  = 1,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $thumprint = $valentia.certificate.Encrypt.ThumPrint, 
+
+        [parameter(
+            mandatory = 0,
+            position  = 1,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $certPath = $valentia.certificate.Encrypt.CertPath
+    )
+
+    process
+    {
+        foreach ($cred in $Credential)
+        {
+            $passwordByte = [Text.Encoding]::UTF8.GetBytes($Cred.GetNetworkCredential().Password) 
+            $contentInfo  = New-Object Security.Cryptography.Pkcs.ContentInfo @(,$passwordByte) 
+            $EnvelopedCms = New-Object Security.Cryptography.Pkcs.EnvelopedCms $contentInfo
+            $EnvelopedCms.Encrypt((New-Object System.Security.Cryptography.Pkcs.CmsRecipient($Cert))) 
+            [Convert]::ToBase64String($EnvelopedCms.Encode())
+        }
+    }
+
+    begin
+    {
+        try
+        {
+            Add-type –AssemblyName System.Security
+        }
+        catch
+        {
+        }
+
+        $Path = Join-Path $certPath $thumprint
+        if (Test-Path $Path)
+        {
+            $Cert = Get-Item $Path
+        }
+        else
+        {
+            Write-Warning ("Certification not found exception!! Cert: '{0}'" -f $Path)
+        }
+    }
+}
