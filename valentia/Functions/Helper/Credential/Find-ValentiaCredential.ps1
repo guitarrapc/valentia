1cea7718155f677028f7446a96c781c713b8abbc
guitarrapc <cherishacquire@outlook.com>
2016-02-29 03:09:58 +0900

    Remove not use parameters

--- valentia/Functions/Helper/Credential/Find-ValentiaCredential.ps1
+++ valentia/Functions/Helper/Credential/Find-ValentiaCredential.ps1
@@ -5,19 +5,7 @@ function Find-ValentiaCredential
     [OutputType([PSCredential])]
     [CmdletBinding()]
     param
-    (
-        [Parameter(mandatory = $false, position = 0)]
-        [ValidateNotNullOrEmpty()]
-        [string]$TargetName = $valentia.name,
-
-        [Parameter(mandatory = $false, position = 1)]
-        [ValidateNotNullOrEmpty()]
-        [Valentia.CS.CredType]$Type = [Valentia.CS.CredType]::Generic,
-
-        [Parameter(mandatory = $false, position = 2)]
-        [ValidateNotNullOrEmpty()]
-        [string]$AsUserName = ""
-    )
+    ()
     
     return [Valentia.CS.CredentialManager]::List();
 }
